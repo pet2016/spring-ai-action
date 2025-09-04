5e0fd087f424dda5a1d015b18d84051e2a984afe
pet2016 <18022887432@163.com>
2025-09-04 11:21:43 +0800

    add prorject for spring ai action

---
+++ src/main/java/com/sanyao/springaiaction/SpringaiactionApplication.java
@@ -0,0 +1,13 @@
+package com.sanyao.springaiaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanyao/springaiaction/SpringaiactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanyao.springaiaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
